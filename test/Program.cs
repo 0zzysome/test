4543e8c09f14d6c03962f32784279e3cec8181b1
0zzysome <oscar.rosin@elev.ga.ntig.se>
2022-08-30 12:56:01 +0200

    lhigrs-ljidfrsg.lijhdfbg.lih

--- test/Program.cs
+++ test/Program.cs
@@ -1,15 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
 
 Wisp w1 = new Wisp();
 Wisp w2 = new Wisp();
 WanderingVagrant v1 = new WanderingVagrant();
 
-v1.Name = "mending";
+v1.Name = "Mending ";
 v1.Health = 1000;
 
 
-while(v1.Health <= 0){
+while(v1.Health > 0){
     string input = Console.ReadLine();
 
     int dmg;
@@ -18,8 +18,8 @@ while(v1.Health <= 0){
     if (success)
     {
     v1.Health -= dmg;
-    Console.WriteLine(v1.Health);
-
+    Console.WriteLine("Enemy "+v1.Name+"Wandering Vagrant has "+v1.Health+" hp left.");
+    
     }
 
 
